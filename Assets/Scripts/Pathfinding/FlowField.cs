dc485afbf489448f16b884d33f09d9ee51225725
unknown <lozman1@elne.org>
2019-11-25 14:02:06 -0500

    base code

--- Assets/Scripts/Pathfinding/FlowField.cs
+++ Assets/Scripts/Pathfinding/FlowField.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+ * I'll explain the algorithm in detail in the Github wiki
+ */
 public class FlowField : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int mapSize = 32;
+
+    FlowGrid[][] subGrids;
+    LinkedList<int>[] adjacencyList;
+
+    int targetX, targetY;
+}
+
+public class FlowGrid
+{
+
+    public static int gridResolution = 24;
+
+    int[][] costField;//TODO figure out what the best way to pull the cost field from the map.
+    int[][] integration_field;
+    int[][] direction_field;
+
+    public FlowGrid()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void compute()
     {
-        
+        //Assign target squares before calling compute.
+
     }
 }
