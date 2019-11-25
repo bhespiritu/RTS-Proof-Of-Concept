a604d25606c5b8dddfd7a6ca31b95e1125c02d44
Block Head <lozman1@elne.org>
2019-11-24 21:58:03 -0500

    Basic Scaffolding Code

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    public int health;
+
+    public static float moveSpeed;
+    public static float turnSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
