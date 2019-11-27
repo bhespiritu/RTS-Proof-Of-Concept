cf812cb5d0056d363d12b4b0f7b353155f16173d
Roger Clanton <rogermclanton@gmail.com>
2019-11-26 21:27:52 -0500

    Added to unit methods and variables

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -9,6 +9,16 @@ public class Unit : MonoBehaviour
 
     public static float moveSpeed;
     public static float turnSpeed;
+    public static float energyConsumption;
+    public static float visionRadius;
+    public static float radarRadius;
+    public int killCount;
+
+    //Weapon information need this for each weapon
+    public float dps;
+    public float projectileDmg;
+    public float reloadTime;
+    public float WeaponRange;
 
 
     // Start is called before the first frame update
@@ -22,4 +32,20 @@ public class Unit : MonoBehaviour
     {
         
     }
+
+    //I'm going to start adding things I think we need. Feel free to correct 
+    //@Author Roger Clanton
+    void takeDamage(int dmg){
+        health -= dmg;
+        if (health <= 0){
+            unitDeath();
+        }
+    }
+
+    //Destroys the unit. Notifies unit manager
+    void unitDeath(){
+
+    }
+
+
 }
