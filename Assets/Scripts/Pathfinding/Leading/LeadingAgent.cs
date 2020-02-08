e2dfcf294e8928dc7c4be8d4f438d6e235c83f7b
Block Head <lozman1@elne.org>
2020-02-08 17:17:03 -0500

    leading algorithm

---
+++ Assets/Scripts/Pathfinding/Leading/LeadingAgent.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeadingAgent : MonoBehaviour
+{
+
+    public GameObject debugMarker;
+
+    public float turningSpeed = 180; //in degrees per second
+    public float moveSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private float dumbass = 0;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetDirection = debugMarker.transform.position - transform.position;
+        float currentAngle = Vector3.SignedAngle(transform.forward, targetDirection,Vector3.up);
+        if (Mathf.Abs(currentAngle) >= turningSpeed * Time.fixedDeltaTime)
+        {
+            float sign = Mathf.Sign(currentAngle);
+            Vector3 rot = transform.localEulerAngles;
+            rot.y += sign * turningSpeed * Time.fixedDeltaTime;
+            transform.localEulerAngles = rot;
+        } else
+        {
+            transform.forward = targetDirection;
+        }
+
+        if(targetDirection.sqrMagnitude > 1)
+            transform.position += transform.forward * moveSpeed * Time.fixedDeltaTime * (1-(Mathf.Abs(currentAngle)/180));
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(transform.position, transform.forward);
+    }
+}
