b10ebceb1fa7d1e212c95aad23bb58af73b2e9f6
NamelessTri <65321652+NamelessTri@users.noreply.github.com>
2020-05-14 16:08:46 -0400

    Added Test Particle Collider

---
+++ Assets/Scripts/Particle/ProjectileCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileCollider : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts) {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2) { 
+            Debug.Log("COLLISION DETECTED");
+            Destroy(gameObject);
+            //TODO: Add any interactions with the collisions here
+        }
+    }
+}
