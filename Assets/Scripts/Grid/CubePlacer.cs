f6ab441f6fa492668dc28b4455fc0623c23a73f0
Roger Clanton <rogermclanton@gmail.com>
2020-05-13 22:48:27 -0400

    Started Work on building placement cursors

---
+++ Assets/Scripts/Grid/CubePlacer.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlacer : MonoBehaviour
+{
+
+    private GridR grid;
+
+    public Material placeable;
+    public Material unplaceable;
+
+    public GameObject buildingPrefab;
+
+    public GameObject cursor;
+
+    private MeshRenderer mesh;
+    private MeshRenderer childMesh;
+
+    public LayerMask placementLayerMask;
+
+    private void Awake(){
+      
+        grid = FindObjectOfType<GridR>();
+        cursor = Instantiate(buildingPrefab);
+        mesh = cursor.GetComponent<MeshRenderer>();
+        cursor.layer = 8;
+
+        foreach (Transform child in cursor.transform)
+        {
+            child.gameObject.layer = 8;
+            childMesh = child.GetComponent<MeshRenderer>();
+            childMesh.material = placeable;
+        }
+
+
+    }
+
+    private void Update(){
+
+        
+
+        RaycastHit hitInfo;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hitInfo,Mathf.Infinity,placementLayerMask))
+        {
+            cursor.transform.position = hitInfo.point;
+            mesh.material = placeable;
+            foreach (Transform child in cursor.transform)
+            {
+                child.gameObject.layer = 8;
+                childMesh = child.GetComponent<MeshRenderer>();
+                childMesh.material = placeable;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0)){
+            
+            if(Physics.Raycast(ray, out hitInfo, Mathf.Infinity, placementLayerMask)){
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+        
+
+    }
+
+    
+    private void PlaceCubeNear(Vector3 clickPoint){
+        var finalPosition = grid.getGridPoint(clickPoint);
+        RaycastHit hitInfo;
+        GameObject cube = GameObject.Instantiate(buildingPrefab);
+        cube.transform.position = finalPosition;
+        Physics.Raycast(cube.transform.position + 10*Vector3.up, Vector3.down, out hitInfo);
+        cube.transform.up = hitInfo.normal;
+        cube.transform.position +=  hitInfo.normal * 0.5f; 
+    }
+}
---
+++ Assets/Scripts/Grid/GridR.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridR : MonoBehaviour
+{
+
+
+    // # of units between grid points
+    [SerializeField]
+    private float spacing = 1f;
+
+    public Vector3 getGridPoint(Vector3 position){
+        //Subtract off its coordinates before multiplication
+        position -= transform.position;
+
+        int xTranformations = Mathf.RoundToInt(position.x / spacing);
+        int yTranformations = Mathf.RoundToInt(position.y / spacing);
+        int zTranformations = Mathf.RoundToInt(position.z / spacing);
+
+        Vector3 newVector = new Vector3( (float)xTranformations * spacing, 
+            (float)yTranformations * spacing, (float)zTranformations * spacing);  
+
+        newVector += transform.position;
+
+        return newVector;
+    
+     }
+   
+   //Test to draw spheres on the points of the grid
+   private void OnDrawGizmos(){
+       Gizmos.color = Color.yellow;
+       for(float x = 0; x<50; x+= spacing){
+           for(float y = 0; y<50; y+= spacing){
+               var dot = getGridPoint(new Vector3(x,0f,y));
+               Gizmos.DrawSphere(dot,.1f);
+           }
+       }
+   }
+}
