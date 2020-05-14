8557302429f53f94c647487b3aa346855b030a32
MaxWiz <max.wiz.wiz@gmail.com>
2020-05-14 13:55:59 -0400

    Fixed Kinetic Aiming
    Made Kinetic Weapons work across open spaces and over obstacles for static targets

--- Assets/Scripts/Aiming/KineticWeapons.cs
+++ Assets/Scripts/Aiming/KineticWeapons.cs
@@ -12,25 +12,23 @@ public class KineticWeapons : MonoBehaviour
     {
     }
 
-    bool straightLine(Vector3 v1, Vector3 v2)
-    {
-        return true;
-    }
-
-    Vector3 trajectory1(Vector3 v1, Vector3 v2)
+    Vector3 maximumMobileTrajectory(Vector3 v1, Vector3 v2, Vector3 targetVelocity)
     {
         float grav = 9.8f;
-        float projVelo = 200.0f;
+        float projVelo = 20.0f;
         Vector3 diff = v2 - v1;
+        float h = -diff.y;
         diff.y = 0;
+        float d = diff.magnitude;
+        float a = (grav * Mathf.Pow(d, 2)) / (2 * Mathf.Pow(projVelo, 2));
+        float theta = (Mathf.Acos(((2 * a) - h) / Mathf.Sqrt(Mathf.Pow(h, 2) + Mathf.Pow(d, 2))) + Mathf.Atan2(d, h)) / 2;
         diff = diff.normalized;
-        float val = (4 * Mathf.Sqrt(v1.y + (Mathf.Pow(projVelo, 2) / (2 * grav)) - v2.y));
-        float theta = Mathf.Atan(1/val);
+        diff = diff * projVelo * Mathf.Cos(theta);
         diff.y = projVelo * Mathf.Sin(theta);
         return diff;
     }
 
-    Vector3 trajectory2(Vector3 v1, Vector3 v2)
+    Vector3 maximumStaticTrajectory(Vector3 v1, Vector3 v2)
     {
         float grav = 9.8f;
         float projVelo = 20.0f;
@@ -40,14 +38,26 @@ public class KineticWeapons : MonoBehaviour
         float d = diff.magnitude;
         float a = (grav * Mathf.Pow(d, 2)) / (2 * Mathf.Pow(projVelo, 2));
         float theta = (Mathf.Acos(((2 * a) - h) / Mathf.Sqrt(Mathf.Pow(h, 2) + Mathf.Pow(d, 2))) + Mathf.Atan2(d, h))/2;
-        Debug.Log(Mathf.Rad2Deg*theta);
+        theta = (Mathf.PI / 2) - theta - Mathf.Sin(h/d);
         diff = diff.normalized;
         diff = diff * projVelo * Mathf.Cos(theta);
-/*        if (diff.z > 0)
-        {
-            diff.z = -diff.z;
-            Debug.Log("error");
-        }*/
+        diff.y = projVelo * Mathf.Sin(theta);
+        return diff;
+    }
+
+    Vector3 minimumStaticTrajectory(Vector3 v1, Vector3 v2)
+    {
+        float grav = 9.8f;
+        float projVelo = 20.0f;
+        Vector3 diff = v2 - v1;
+        float h = -diff.y;
+        diff.y = 0;
+        float d = diff.magnitude;
+        float a = (grav * Mathf.Pow(d, 2)) / (2 * Mathf.Pow(projVelo, 2));
+        float theta = (Mathf.Acos(((2 * a) - h) / Mathf.Sqrt(Mathf.Pow(h, 2) + Mathf.Pow(d, 2))) + Mathf.Atan2(d, h)) / 2;
+        //theta = (Mathf.PI / 2) - theta + Mathf.Atan2(d, h);
+        diff = diff.normalized;
+        diff = diff * projVelo * Mathf.Cos(theta);
         diff.y = projVelo * Mathf.Sin(theta);
         return diff;
     }
@@ -57,17 +67,23 @@ public class KineticWeapons : MonoBehaviour
     {
         Vector3 v1 = transform.position;
         Vector3 v2 = target.transform.position;
-        Vector3 v4 = trajectory2(v1, v2);
+        Vector3 v3 = maximumStaticTrajectory(v1, v2);
+        Vector3 v4 = minimumStaticTrajectory(v1, v2);
         Vector3 diff = v2 - v1;
         Debug.DrawRay(transform.position, diff, Color.blue);
-        //Debug.DrawRay(transform.position, 5 * v4.normalized, Color.green);
-        Debug.DrawRay(transform.position, v4, Color.green);
+        Debug.DrawRay(transform.position, 5 * v4.normalized, Color.green);
+        Debug.DrawRay(transform.position, 5 * v3.normalized, Color.red);
         if (Input.GetMouseButtonDown(0))
         {
             GameObject instance = Instantiate(bullet, transform.position, Quaternion.identity);
             Rigidbody r = instance.GetComponent<Rigidbody>();
-            Debug.Log(v4);
             r.velocity = v4;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject instance = Instantiate(bullet, transform.position, Quaternion.identity);
+            Rigidbody r = instance.GetComponent<Rigidbody>();
+            r.velocity = v3;
         }
     }
 }
