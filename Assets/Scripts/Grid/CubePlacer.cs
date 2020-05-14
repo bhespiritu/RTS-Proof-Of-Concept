38566f37b9c7b4d13d08410d40aacebb75eedb21
Roger Clanton <rogermclanton@gmail.com>
2020-05-14 00:12:32 -0400

    Updated Building Placement

--- Assets/Scripts/Grid/CubePlacer.cs
+++ Assets/Scripts/Grid/CubePlacer.cs
@@ -19,13 +19,17 @@ public class CubePlacer : MonoBehaviour
 
     public LayerMask placementLayerMask;
 
+    public LayerMask buildingsLayerMask;
+
     private void Awake(){
       
+        //Find the grid, stores information about whether a space is placeable
         grid = FindObjectOfType<GridR>();
+        //Glowy construct of where the building will be placed
         cursor = Instantiate(buildingPrefab);
+        //Layers and mesh for cursor
         mesh = cursor.GetComponent<MeshRenderer>();
         cursor.layer = 8;
-
         foreach (Transform child in cursor.transform)
         {
             child.gameObject.layer = 8;
@@ -37,18 +41,25 @@ public class CubePlacer : MonoBehaviour
     }
 
     private void Update(){
+
+        //Shoot a ray to the mouse
         RaycastHit hitInfo;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
+        
+        //set local so the cursor will keep updating
         Vector3 localUp = cursor.transform.up;
         Vector3 localForward = cursor.transform.forward;
 
+        //If the cursor is above a placable terrain
         if (Physics.Raycast(ray, out hitInfo,Mathf.Infinity,placementLayerMask))
         {
             
+            //Snap to the grid
             cursor.transform.position = grid.getGridPoint(hitInfo.point);
             localUp = hitInfo.normal;
             cursor.transform.position += hitInfo.normal * 0.5f;
+
+            //Set the material of the cursor to the placeable color
             mesh.material = placeable;
             foreach (Transform child in cursor.transform)
             {
@@ -69,7 +80,21 @@ public class CubePlacer : MonoBehaviour
 
         cursor.transform.LookAt(cursor.transform.position + localForward, localUp);
 
-        if (Input.GetMouseButtonDown(0)){
+        Ray downRay = new Ray(cursor.transform.position, Vector3.down);
+        Debug.DrawRay(cursor.transform.position, Vector3.down);
+
+        if (Physics.Raycast(downRay, out hitInfo, Mathf.Infinity, buildingsLayerMask))
+        {
+            mesh.material = unplaceable;
+            foreach (Transform child in cursor.transform)
+            {
+                child.gameObject.layer = 8;
+                childMesh = child.GetComponent<MeshRenderer>();
+                childMesh.material = unplaceable;
+            }
+        }
+
+            if (Input.GetMouseButtonDown(0)){
             
             if(Physics.Raycast(ray, out hitInfo, Mathf.Infinity, placementLayerMask)){
                 PlaceCubeNear();
