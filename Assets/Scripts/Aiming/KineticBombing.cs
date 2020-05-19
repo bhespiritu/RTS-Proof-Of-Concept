3b7957ef41ab8b3f4de9efc0b181de4d36997369
MaxWiz <max.wiz.wiz@gmail.com>
2020-05-18 23:07:51 -0400

    Small Changes

---
+++ Assets/Scripts/Aiming/KineticBombing.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KineticBombing : MonoBehaviour
+{
+    public GameObject target;
+    public Rigidbody targetBody;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float dropTime(Vector3 v1, Vector3 v2, Vector3 targetVelocity)
+    {
+        float grav = 9.8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
