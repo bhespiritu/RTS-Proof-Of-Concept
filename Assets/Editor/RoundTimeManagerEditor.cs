0a9c83843a94e556d75c814a08ad410b5b96521b
bhespiritu <lozman1@elne.org>
2020-05-22 23:06:45 -0400

    Timing System Update and Example

---
+++ Assets/Editor/RoundTimeManagerEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoundTimeManager))]
+public class RoundTimeManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Start Round"))
+        {
+            ((RoundTimeManager)target).StartRound();
+        }
+    }
+}
---
+++ Assets/Scripts/Timing/TimingExample.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimingExample : MonoBehaviour
+{
+
+    void OnEnable()
+    {
+        RoundTimeManager.OnRoundStart += OnStart;
+        RoundTimeManager.OnRoundTick += OnTick;
+    }
+
+
+    void OnDisable()
+    {
+        RoundTimeManager.OnRoundStart -= OnStart;
+        RoundTimeManager.OnRoundTick -= OnTick;
+    }
+
+    public void OnStart()
+    {
+        Debug.Log("I AM ALIVE");
+    }
+
+    public void OnTick()
+    {
+        gameObject.transform.position = Vector3.up * (Mathf.Sin(RoundTimeManager.INSTANCE.currentTime) + 2);
+    }
+
+}
